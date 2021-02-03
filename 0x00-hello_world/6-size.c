bf4b5880aafedc363914e1d2d14f9f2bba370d3e
Biruk-GD <birukgizaw115@gmail.com>
2021-02-03 20:58:23 +0000

    print

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* sizeof*/
+main()
+{
+  char   ch = ' ';
+  int    int_num = 0;
+  float  flt_num = 0.0f;
+  long int long_int_num = 0;
+  long long int long_long_int_num = 0;
+  printf("the sieof a char is: %d-byte\n", sizeof(char));
+  printf("the sizeof an int: %d-byte \n", sizeof(int));
+  printf("the sizeof a float is : %d-byte \n", sizeof(float));
+  printf("the sizeof a long int is:%d-byte \n",sizeof(long int));
+  printf("the size0f a lomng long int is:%d-byte \n" sizeof long long int));
+return (0);
+}
