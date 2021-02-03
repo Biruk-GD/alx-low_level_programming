8470b29cf9e5e1a074e7340c09b7fb8d3b27d6df
Biruk-GD <birukgizaw115@gmail.com>
2021-02-03 20:16:49 +0000

    Print statment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-puts(""programming is like building a multilingual puzzle\n")
+#include <string.h>
+
+int main ()
+
+{
+puts(""programming is like building a multilingual puzzle \n");
+return(0);
 
 
-
-
+}
