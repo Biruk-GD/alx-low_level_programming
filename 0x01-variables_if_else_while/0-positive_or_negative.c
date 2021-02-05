26e384b510f21d7d101b6aaaa4153f5e06a3984b
Biruk-GD <birukgizaw115@gmail.com>
2021-02-05 12:00:58 +0000

    what is the number?

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
-/*more headers goes there */
-
-/*betty style doc for function main goes there */
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
 int main(void)
 {
 int n;
@@ -11,15 +13,15 @@ n = rand() - RAND_MAX / 2;
 /* my code*/
 if (n > 0)
 {
-printf(" %d is positive \n", &n);
+printf(" %d is positive \n", n);
 }
 if (n == 0)
 {
-printf(" %d is zero\n", &n);
+printf(" %d is zero\n", n);
 }
 if (n < 0)
 {
-printf(" %d is negative 'n", &n);
+printf(" %d is negative \n", n);
 }
 return (0);
 }
