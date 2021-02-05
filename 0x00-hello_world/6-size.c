81fe65461f38bce7d9587491aaf81dad8025aa66
Biruk-GD <birukgizaw115@gmail.com>
2021-02-05 05:03:17 +0000

    edited size of varies types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@ int    int_num = 0;
 float  flt_num = 0.0f;
 long int long_int_num = 0;
 long long int long_long_int_num = 0;
-printf("Size of a char: %d byte(s) \n", sizeof(ch));
+printf("Size of a char: %s byte(s) \n", sizeof(ch));
 printf("Size of an int: %d byte(s) \n", sizeof(int_num));
 printf("Size of a long int: %d byte(s) \n", sizeof(long_int_num));
 printf("Size of a long long int: %d byte(s) \n", sizeof(long_long_int_num));
-printf("Size of a float: %d byte(s) \n", sizeof(flt_num));
+printf("Size of a float: %5.2f byte(s) \n", sizeof(flt_num));
 return (0);
 }
