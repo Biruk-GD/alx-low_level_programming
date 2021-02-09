628d35e5744f4cf46d2716366ac4e90dca80563b
Biruk-GD <birukgizaw115@gmail.com>
2021-02-09 06:53:42 +0000

    print Holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,12 +1,12 @@
 #include <holberton.h>
 
-/** 
+/**
  * main - entry point
- * Return always 0 (success)
+ * Return: always 0 (success)
  */
 
 int main(void)
 {
 _putchar("Holberton\n");
-return(0);
+return (0);
 }
