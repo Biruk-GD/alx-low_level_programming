9034a735b156eefb041716a44663b8feb2f33045
Biruk-GD <birukgizaw115@gmail.com>
2021-02-09 19:38:19 +0000

    last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - prints last digit of a number
+ * @k : int checked
+ * Return: the value of the last digit
+ */
+
+int print_last_digit(int k)
+{
+int r;
+r = k % 10;
+return (r);
+}
