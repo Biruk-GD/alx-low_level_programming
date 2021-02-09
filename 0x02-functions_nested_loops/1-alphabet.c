7c908f6b4d58ccdc69a7d785eb3ab950307c1859
Biruk-GD <birukgizaw115@gmail.com>
2021-02-09 12:13:13 +0000

    print the alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * main - entry point
+ * Return: always 0 (success)
+ */
+
+void print_alphabet(void);
+
+int main(void);
+{
+char x;
+
+for (x = 'a'; x <= 'z'; x++)
+
+_putchar(x);
+
+_putchar('\n');
+return (0);
+}
