7d609a5adf2013bc0efa22fbcf9f093be7608141
Biruk-GD <birukgizaw115@gmail.com>
2021-02-09 11:01:45 +0000

    Print Holberton using _putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,21 +1,23 @@
-#include <holberton.h>
+#include "holberton.h"
 
 /**
  * main - entry point
  * Return: always 0 (success)
  */
 
-char main (void)
+int main(void)
 {
-_putchar(&H);
-_putchar(&o);
-_putchar(&l);
-_putchar(&b);
-_putchar(&e);
-_putchar(&r);
-_putchar(&t);
-_putchar(&0);
-_putchar(&n);
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
 return (0);
+
 }
 
