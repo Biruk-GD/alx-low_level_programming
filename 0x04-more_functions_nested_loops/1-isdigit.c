ed7a3f313985d0d0de8ff31181f994e3db5a080b
Biruk-GD <birukgizaw115@gmail.com>
2021-02-11 15:27:59 +0000

    checks for a digit (0 through 9).

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - returns 1 if c is a digit
+ * @c : int chechked
+ * Return: Always 0.
+ */
+
+int _isdigit(int c)
+
+{
+if (c >= 48 && c <= 58)
+return (1);
+else
+return (0);
+}
