0754251832f04a67d234ed4f2739ee50bd64af0a
Biruk-GD <birukgizaw115@gmail.com>
2021-02-20 05:59:20 +0000

    update value of a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - updates the value it points to to 98
+ * @n: checked int
+ * Return: always 0 (success)
+ */
+
+void reset_to_98(int *n);
+{
+int n;
+int *p = &n;
+*p = 98;
+}
