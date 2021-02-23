2dc6b61647fc71911e8d8c7bc4165c267b823c4d
Biruk-GD <birukgizaw115@gmail.com>
2021-02-23 21:02:01 +0000

    sets the value of a pointer to a char.

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * set_string - locates a character in a string.
+ * @s: A pointer to the memory area to be filled.
+ * @to: cheched char
+ * Return: memory
+ */
+
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
