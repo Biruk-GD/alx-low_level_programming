fb3593fc79437d4bd62c056ed6b28d6b18aed450
Biruk-GD <birukgizaw115@gmail.com>
2021-02-23 12:12:19 +0000

    encodes a string using rot13.

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * *rot13 - encodes a string using rot13.
+ * @s : checked char
+ * Return: always 0 (success)
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+
+	char src[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char dest[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (src[j] == *(s + i))
+			{
+				*(s + i) = dest[j];
+	   
+				break;
+			}
+		}
+	}
+	return (s);
+}
