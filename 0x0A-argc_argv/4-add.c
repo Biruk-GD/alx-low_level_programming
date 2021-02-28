469bce2eb6636293f63ae2f73b1842d5b57b9fa9
Biruk-GD <birukgizaw115@gmail.com>
2021-02-28 16:39:16 +0000

    adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,22 +10,20 @@
 
 int main(int argc, char  *argv[])
 {
-        int num, num2, sum;
+	int num, num2, sum;
 
-        for (num = 1; num < argc; num++)
-        {
-                for (num2 = 0; argv[num][num2]; num2++)
-                {
-                        if (argv[num][num2] < '0' || argv[num][num2] > '9')
-                        {
-
+	for (num = 1; num < argc; num++)
+	{
+		for (num2 = 0; argv[num][num2]; num2++)
+		{
+			if (argv[num][num2] < '0' || argv[num][num2] > '9')
+			{
 				printf("Error\n");
 				return (1);
-                        }
-                }
-                sum += atoi(argv[num]);
-        }
-        printf("%d\n", sum);
-
-        return (0);
+			}
+		}
+		sum += atoi(argv[num]);
+	}
+	printf("%d\n", sum);
+	return (0);
 }
