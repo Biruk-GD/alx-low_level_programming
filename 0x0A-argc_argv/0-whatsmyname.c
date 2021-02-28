31a55e2b2f9d3848bb7248e72386ce62bd63defd
Biruk-GD <birukgizaw115@gmail.com>
2021-02-28 09:30:04 +0000

    program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,13 +2,13 @@
 
 /**
  * main - prints file's name
- * @argc: number of arguments 
+ * @argc: number of arguments
  * @argv: an array of pointers to the strings which are thos arguments
  * Return: memory
  */
 
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-  printf("%s\n", argv[0]);
-  return (0);
+	printf("%s\n", argv[0]);
+	return (0);
 }
