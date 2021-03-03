51fd061d7cc91f4073c82924a90ddc1f8d3dc2f0
Biruk-GD <birukgizaw115@gmail.com>
2021-03-03 18:48:40 +0000

    Concatenates two strings.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,9 +18,9 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	}
 	if (s2 == 0)
-        {
-                s2 = "";
-        }
+	{
+		s2 = "";
+	}
 	len1 = 0;
 	while (*(s1 + len1))
 	{
@@ -28,9 +28,9 @@ char *str_concat(char *s1, char *s2)
 	}
 	len2++;
 	while (*(s2 + len2))
-        {
-                len2++;
-        }
+	{
+		len2++;
+	}
 	s = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (s == 0)
 	{
@@ -41,9 +41,9 @@ char *str_concat(char *s1, char *s2)
 		*(s + index) = *(s1 + index);
 	}
 	for (index2 = 0; index2 < len2; index2++, index++)
-        {
-                *(s + index) = *(s2 + index2);
-        }
+	{
+		*(s + index) = *(s2 + index2);
+	}
 	*(s + (len1 + len2)) = '\0';
 	return (s);
 }
